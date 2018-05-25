328514313c5a26db4e265aaf8cb4184227ec7e5c
Kim LeBlanc <khleblanc@gmail.com>
2018-05-24 23:13:52 -0400

    Fixed the python script to do what it is supposed to (merge a bunch of csv files into one dataframe), .... The key problem to over-come was convincing the pandas read_csv method to use a normal encoding instead of utf-8 for the input data files.

--- Merge2018CSVs.py
+++ Merge2018CSVs.py
@@ -6,15 +6,21 @@ Created on Thu May 24 14:22:45 2018
 """
 
 import pandas as pd
-import numpy as np
+#import numpy as np
 import os
-import sys
+#import sys
 
-FileDir ="C:/Users/leblanckh/Downloads/ExPORTER_CSVs_2011to2018/RePORTER_FY18"
+FileDir ="/Users/leblanckh/gitProjects/MetaScare/RePORTER_FY18"
 os.chdir(FileDir)
 FileList = os.listdir(FileDir)
-#frame = pd.DataFrame()
-#dfList = []
 
-df = pd.concat((pd.read_csv(File) for File in FileList))
+#Simple Test with just 2 dataFrames
+#frame1 = pd.read_csv(FileList[0], encoding="ISO-8859-1")
+#frame2 = pd.read_csv(FileList[1], encoding="ISO-8859-1")
+#dfList = [frame1, frame2]
+#print("Point 3", dfList)
+#df = pd.concat(dfList)
+
+#cool one-line for each to not make an intermediate list object
+df = pd.concat((pd.read_csv(File, encoding="ISO-8859-1") for File in FileList))
 print (df.head())
