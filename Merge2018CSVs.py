cdea05b564914c8caae1c7b479e8193163e8afa0
Kim LeBlanc <khleblanc@gmail.com>
2018-05-26 22:51:15 -0400

    Updated Merge2018CSVs.py to handle a list of input file directories for merging and to name the resulting file after the .... directory that was merged.  Also added the final output file of that result as well as each of the input files for the new directory that needed to merged.

--- Merge2018CSVs.py
+++ Merge2018CSVs.py
@@ -10,21 +10,22 @@ import pandas as pd
 import os
 #import sys
 
-FileDir = r"C:\Users\leblanckh\gitRepos\MetaScare\RePORTER_FY18"
-os.chdir(FileDir)
-FileList = os.listdir(FileDir)
+#FileDirList = r"C:\Users\leblanckh\gitRepos\MetaScare\RePORTER_PRJ_C_FY2018"
+#ProjectTop=r"C:\Users\leblanckh\gitRepos\MetaScare"
+ProjectTop="/Users/leblanckh/gitProjects/MetaScare"
 
-#Simple Test with just 2 dataFrames
-#frame1 = pd.read_csv(FileList[0], encoding="ISO-8859-1")
-#frame2 = pd.read_csv(FileList[1], encoding="ISO-8859-1")
-#dfList = [frame1, frame2]
-#print("Point 3", dfList)
-#df = pd.concat(dfList)
+dirNameList = ["RePORTER_PRJ_C_FY2018", "RePORTER_PRJABS_C_FY2018"]
+for Name in dirNameList:
+    Dir = ProjectTop + "/" + Name
+    os.chdir(Dir)
+    FileList = os.listdir(Dir)
 
-#cool one-line for each to not make an intermediate list object
-df = pd.concat((pd.read_csv(File, encoding="ISO-8859-1") for File in FileList))
-print (df.head())
+    #cool one-line for each to not make an intermediate list object
+    df = pd.concat((pd.read_csv(File, encoding="ISO-8859-1") for File in FileList))
+    #print (df.head())
 
-#write to csv in the MetaScare directory
-os.chdir(r"C:\Users\leblanckh\gitRepos\MetaScare")
-df.to_csv("RePORTER_PRJ_C_FY2018.csv")
+    #write to csv in the MetaScare directory
+    #os.chdir(r"C:\Users\leblanckh\gitRepos\MetaScare")
+    os.chdir(ProjectTop)
+    newFileName = Name + ".csv"
+    df.to_csv(newFileName)
