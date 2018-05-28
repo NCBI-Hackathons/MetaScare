30d24bc3bf489d41581d952c1dcef4d9a4c318c9
Kim LeBlanc <khleblanc@gmail.com>
2018-05-27 23:16:31 -0400

    added a sort on Application ID

--- Merge2018CSVs.py
+++ Merge2018CSVs.py
@@ -10,7 +10,7 @@ import pandas as pd
 import os
 #import sys
 
-#FileDirList = r"C:\Users\leblanckh\gitRepos\MetaScare\RePORTER_PRJ_C_FY2018"
+#If on Windows, switch ProjectTop
 #ProjectTop=r"C:\Users\leblanckh\gitRepos\MetaScare"
 ProjectTop="/Users/leblanckh/gitProjects/MetaScare"
 
@@ -22,14 +22,13 @@ for Name in dirNameList:
 
     #cool one-line for each to not make an intermediate list object
     df = pd.concat((pd.read_csv(File, encoding="ISO-8859-1") for File in FileList))
-    #print (df.head())
-
+    print (df.head())
+    #sort Project file on Award notice date and Application ID
+    if Name == "RePORTER_PRJ_C_FY2018":
+        df = df.sort_values(by = ['APPLICATION_ID'])
+        print (df.head())
     #write to csv in the MetaScare directory
-    #os.chdir(r"C:\Users\leblanckh\gitRepos\MetaScare")
     os.chdir(ProjectTop)
     newFileName = Name + ".csv"
     df.to_csv(newFileName)
-    print("Column Labels:")
-    print(df.columns.values.tolist())
-    print("Column Head:"
-    print(df.head())
+    
